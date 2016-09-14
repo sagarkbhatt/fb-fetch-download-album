e38017990327f33a51cf8027fd910a7133c442f5
sagarkbhatt <sagarkbhatt@yahoo.com>
2016-09-14 20:30:45 +0530

    #Upload

---
+++ download.php
@@ -0,0 +1,48 @@
+<?php
+
+$errors = array();
+$data = array();
+// Getting posted data and decodeing json
+$postdata = file_get_contents('php://input');
+$request = json_decode($postdata);
+$img = $request;
+
+
+
+//echo $img;
+
+$zip = new ZipArchive();
+
+$filename = 'uploads/'.time().'.zip';
+if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
+    die ("Could not open archive");
+$temp=[];
+foreach($img as $file){
+
+    # download file
+    $download_file = file_get_contents($file);
+    #add it to the zip
+    $name =basename($file);
+    array_push($temp,$name);
+    file_put_contents($name,$download_file);
+
+    $zip->addFile($name);
+
+    
+    
+}
+
+
+$zip->close();
+//echo 'Exist or not'.file_exists($filename);
+
+foreach($temp as $n){
+
+    unlink($n);
+}
+
+echo $filename;
+# close zip
+
+
+?>
