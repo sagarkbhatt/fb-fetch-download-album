50ad1cdea5bfe8353793298b02a06eb454e72443
sagarkbhatt <sagarkbhatt@yahoo.com>
2016-09-17 18:39:22 +0530

    #MultipeDownload

--- download.php
+++ download.php
@@ -5,38 +5,34 @@ $data = array();
 
 $postdata = file_get_contents('php://input');
 $request = json_decode($postdata);
-//var_dump($request);
-//echo $request;
+$img = $request;
+
+
+
 $zip = new ZipArchive();
+
 $filename = 'uploads/'.time().'.zip';
 if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
     die ("Could not open archive");
 
+
 $temp=[];
-foreach($request as $k){
-
-$img=$k->img;
-$albumName=$k->name;
-
-
 foreach($img as $file){
+	
 	
 	$download_file = file_get_contents($file);
 	
 	$name =basename($file);
 	array_push($temp,$name);
 	file_put_contents($name,$download_file);
-	if(!isset($albumName))
+	
 	$zip->addFile($name);
-	else
-	$zip->addFile($name,$albumName.'/'.$name);
-
 	
 	
 	
 }
 
-}
+
 $zip->close();
 //echo 'Exist or not'.file_exists($filename);
 
@@ -44,9 +40,6 @@ foreach($temp as $n){
 	
 	unlink($n);
 }
-
-
-
 $filename=array($filename);
 echo json_encode($filename);
 # close zip
--- downloadFolder.php
+++ downloadFolder.php
@@ -5,34 +5,38 @@ $data = array();
 
 $postdata = file_get_contents('php://input');
 $request = json_decode($postdata);
-$img = $request;
-
-
-
+//var_dump($request);
+//echo $request;
 $zip = new ZipArchive();
-
 $filename = 'uploads/'.time().'.zip';
 if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
     die ("Could not open archive");
 
-
 $temp=[];
+foreach($request as $k){
+
+$img=$k->img;
+$albumName=$k->name;
+
+
 foreach($img as $file){
-	
 	
 	$download_file = file_get_contents($file);
 	
 	$name =basename($file);
 	array_push($temp,$name);
 	file_put_contents($name,$download_file);
-	
+	if(!isset($albumName))
 	$zip->addFile($name);
+	else
+	$zip->addFile($name,$albumName.'/'.$name);
+
 	
 	
 	
 }
 
-
+}
 $zip->close();
 //echo 'Exist or not'.file_exists($filename);
 
@@ -40,6 +44,9 @@ foreach($temp as $n){
 	
 	unlink($n);
 }
+
+
+
 $filename=array($filename);
 echo json_encode($filename);
 # close zip
